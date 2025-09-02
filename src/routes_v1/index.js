d2384d1c418d3cfa9f80ed632233daa19c6892a6
Nishant <official.nishant.ni8@gmail.com>
2025-09-02 22:05:01 +0530

    added health route

--- src/routes_v1/index.js
+++ src/routes_v1/index.js
@@ -3,5 +3,11 @@ import userRoutes from "./users/users_routes.js";
 
 const v1 = express.Router();
 v1.use("/user", userRoutes);
+v1.get(
+  "/health",
+  tryCatchUtil(async (req, res) => {
+    return res.send({ health: "OK" });
+  })
+);
 
 export default v1;
