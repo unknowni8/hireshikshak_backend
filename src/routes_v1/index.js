555aea19b47857cd421e8d65d7ee13a3c226cb97
Nishant <official.nishant.ni8@gmail.com>
2025-09-03 19:02:38 +0530

    fixed import issue

--- src/routes_v1/index.js
+++ src/routes_v1/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./users/users_routes.js";
+import tryCatchUtil from "../utilities/try_catch_util.js";
 
 const v1 = express.Router();
 v1.use("/user", userRoutes);
