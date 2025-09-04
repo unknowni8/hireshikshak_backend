fc33f2fe14d93d6cd3bfe25983830e3e24828d38
Nishant <official.nishant.ni8@gmail.com>
2025-09-04 15:45:12 +0530

    modified getUser response

--- src/routes_v1/users/users_controllers.js
+++ src/routes_v1/users/users_controllers.js
@@ -2,6 +2,6 @@ import tryCatchUtil from "../../utilities/try_catch_util.js";
 
 export const getUser = tryCatchUtil(async (req, res) => {
     const { id } = req.params;
-    return res.send({ data: 'users' });
+    return res.send({ data: `user with id: ${id}` });
 });
 
